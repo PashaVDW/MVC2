f2fc01786d9902c53c01d62a6c9b7fcc79ba0aeb
Cfuzion <pasha.pasha12e@gmail.com>
2023-07-07 14:51:56 +0200

    .htaccess

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace config;
+
+/**
+ * Configuration
+ */
+
+class Config {
+    const DB_HOST = '127.0.0.1:8000';
+    const DB_NAME = 'MVC2';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+}
